cf2fe07a20b9811d30a730a9a0ee634c5269dab3
Konrad Feiler <K_W_F@web.de>
2020-01-30 23:21:46 +0100

    setup with blue background and maker.js example svg

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
-
-import ExampleComponent from 'react-blueprint-svg'
+import Blueprint from 'react-blueprint-svg'
+import SmileModel from './makerjs-smile'
+import makerjs from 'makerjs';
 
 export default class App extends Component {
-  render () {
+
+  render() {
+    let smile = new SmileModel();
+    let svg = makerjs.exporter.toSVG(smile, { origin: [0, 0] });
+
     return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
+      <div className="container">
+        <h3>Example maker.js smiley:</h3>
+        <Blueprint svg={svg} />
       </div>
     )
   }
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,53 @@
 /**
- * @class ExampleComponent
+ * @className Blueprint
  */
 
 import * as React from 'react'
-
 import styles from './styles.css'
 
-export type Props = { text: string }
+export type Props = { svg: string }
 
-export default class ExampleComponent extends React.Component<Props> {
-  render() {
-    const {
-      text
-    } = this.props
+const Blueprint = ({ svg }: Props) => {
 
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
+  return <section className={styles.blueprintCanvas}>
+
+    <svg className={styles.grid}>
+      <defs>
+        <pattern id="pattern1" x="0" y="0" width=".1" height=".1">
+          <line x1="0" y1="0" x2="0" y2="100%" className={styles.gridLine1}></line>
+          <line x1="0" y1="0" x2="100%" y2="0" className={styles.gridLine1}></line>
+        </pattern>
+        <pattern id="pattern10" x="0" y="0" width="1" height="1">
+          <line x1="0" y1="0" x2="0" y2="100%" className={styles.gridLine10}></line>
+          <line x1="0" y1="0" x2="100%" y2="0" className={styles.gridLine10}></line>
+        </pattern>
+        <pattern id="gridPattern" x="0" y="0" width="100" height="100" patternUnits="userSpaceOnUse" patternTransform="translate(0,0)">
+          <rect id="gridPatternFill" fill="url(#pattern1)" width="100" height="100" x="0" y="0"></rect>
+          <rect fill="url(#pattern10)" width="100%" height="100%" x="0" y="0"></rect>
+        </pattern>
+      </defs>
+      <rect fill="url(#gridPattern)" width="100%" height="100%" x="0" y="0"></rect>
+      <g id="crosshairs">
+        <line x1="-100%" x2="100%" y1="0" y2="0"></line>
+        <line x1="0" x2="0" y1="-100%" y2="100%"></line>
+      </g>
+    </svg>
+
+    <div className={styles.viewParams}>
+      <div className={`${styles.view} noselect`} touch-action="none">
+        <div id="view-svg-container">
+          {svg}
+        </div>
+        <svg id="pointers" xmlns="http://www.w3.org/2000/svg"></svg>
+        <div id="touch-shield"></div>
       </div>
-    )
-  }
+      <div id="rendering-options-menu" className={`${styles.view} noselect`}>
+        <div id="params"></div>
+      </div>
+    </div>
+    <div id="notes"></div>
+
+  </section>
 }
+
+export default Blueprint
