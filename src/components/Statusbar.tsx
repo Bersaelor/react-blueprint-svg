36f1245cdb711f83a94629da6bb1bab29d175530
Konrad Feiler <K_W_F@web.de>
2020-01-31 15:34:21 +0100

    get hooks working in the example

--- src/components/Statusbar.tsx
+++ src/components/Statusbar.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react'
 import { IPoint } from 'makerjs'
 import styles from './Statusbar.css'
-// import { useTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next";
 
 export type Props = { point: IPoint, zoom: number }
 
 function Statusbar({ point, zoom }: Props) {
-    // const { i18n } = useTranslation()
+    const { i18n } = useTranslation()
 
     return <div className={styles.statusbar} >
-        { `x: ${ point[0] }, y: ${ point[1] } | zoom: ${ zoom.toLocaleString('en', {style: "percent"}) }` }
+        { `x: ${ point[0] }, y: ${ point[1] } | zoom: ${ zoom.toLocaleString(i18n.language, {style: "percent"}) }` }
     </div>
 }
 
