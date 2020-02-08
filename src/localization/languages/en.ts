4502c1d8c2fadd2fc7452ef9e5a8f21b21381d3d
Konrad Feiler <K_W_F@web.de>
2020-02-08 17:04:41 +0100

    sizing and css issues

--- src/localization/languages/en.ts
+++ src/localization/languages/en.ts
@@ -1,7 +1,7 @@
 export default {
 	"statusbar.zoom": "Zoom:",
 	"blueprint.renderingOptions": "customize",
-	"OptionsMenu.fitOnScreen": "Fit on Scren",
+	"OptionsMenu.fitOnScreen": "Fit on Screen",
 	"OptionsMenu.showGrid": "Show Grid",
 	"OptionsMenu.showPathNames": "Show Path Names",
 	"OptionsMenu.showPathFlow": "Show path flow",
